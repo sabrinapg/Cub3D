317cf0dd0b3c55f23eb9a37f64f3b2c3729d68ce
Mahi Kassa <mahlet.takassa@gmail.com>
2026-09-01 15:29:45 +0200

    update parse_texture src file

--- src/parser/parse_texture.c
+++ src/parser/parse_texture.c
@@ -10,19 +10,79 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../../cub3D.h"
 
-int			parse_texture(char *line, t_scene *scene)
-{
-
-}
-
-int			set_texture_path(char *id, char *path, t_scene *scene)
-{
-
-}
+#include "../../includes/parser.h"
+#include "../../includes/utils.h"
+#include "../../includes/libft.h"
 
 static int	is_texture_identifier(char *id)
 {
+	if (!ft_strncmp(id, ID_NO, 2))
+		return (1);
+	if (!ft_strncmp(id, ID_SO, 2))
+		return (1);
+	if (!ft_strncmp(id, ID_WE, 2))
+		return (1);
+	if (!ft_strncmp(id, ID_EA, 2))
+		return (1);
+	return (0);
+}
 
+static int	skip_texture_spaces(char *line, int index)
+{
+	while (line[index] && ft_isspace_cub(line[index]))
+		index++;
+	return (index);
+}
+
+int	set_texture_path(char *id, char *path, t_scene *scene)
+{
+	char	**texture_path;
+
+	texture_path = NULL;
+	if (!ft_strncmp(id, ID_NO, 2))
+		texture_path = &scene->textures.north_path;
+	else if (!ft_strncmp(id, ID_SO, 2))
+		texture_path = &scene->textures.south_path;
+	else if (!ft_strncmp(id, ID_WE, 2))
+		texture_path = &scene->textures.west_path;
+	else if (!ft_strncmp(id, ID_EA, 2))
+		texture_path = &scene->textures.east_path;
+	if (!texture_path)
+		return (print_error("invalid texture identifier"));
+	if (*texture_path)
+		return (print_error("duplicate texture identifier"));
+	*texture_path = path;
+	return (1);
+}
+
+int	parse_texture(char *line, t_scene *scene)
+{
+	int		index;
+	int		start;
+	char	*path;
+
+	if (!line || !scene || !is_texture_identifier(line))
+		return (print_error("invalid texture line"));
+	index = skip_texture_spaces(line, 2);
+	start = index;
+	while (line[index] && !ft_isspace_cub(line[index]))
+		index++;
+	if (index == start)
+		return (print_error("missing texture path"));
+	path = ft_substr(line, start, index - start);
+	if (!path)
+		return (print_error("memory allocation failed"));
+	index = skip_texture_spaces(line, index);
+	if (line[index])
+	{
+		free(path);
+		return (print_error("texture line has too many arguments"));
+	}
+	if (!set_texture_path(line, path, scene))
+	{
+		free(path);
+		return (0);
+	}
+	return (1);
 }
