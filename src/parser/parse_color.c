5109cb658366e6a975f1909ad3e28332ce351dc0
Mahi Kassa <mahlet.takassa@gmail.com>
2026-09-04 09:53:07 +0200

    update parse_color and parse_map

--- src/parser/parse_color.c
+++ src/parser/parse_color.c
@@ -10,3 +10,93 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+static int	skip_color_spaces(char *line, int index)
+{
+	while (line[index] && ft_isspace_cub(line[index]))
+		index++;
+	return (index);
+}
+
+static int	color_to_int(t_color *color)
+{
+	int	red;
+	int	green;
+	int	blue;
+
+	red = color->r << 16;
+	green = color->g << 8;
+	blue = color->b;
+	return (red + green + blue);
+}
+
+static int	parse_rgb_component(char *value, int *index, int *component)
+{
+	int	number;
+	int	has_digit;
+
+	number = 0;
+	has_digit = 0;
+	while (value[*index] && ft_isdigit(value[*index]))
+	{
+		has_digit = 1;
+		number = (number * 10) + (value[*index] - '0');
+		if (number > RGB_MAX)
+			return (0);
+		*index = *index + 1;
+	}
+	if (!has_digit)
+		return (0);
+	*component = number;
+	return (1);
+}
+
+static int	parse_rgb(char *value, t_color *color)
+{
+	int	index;
+
+	index = 0;
+	if (!parse_rgb_component(value, &index, &color->r))
+		return (0);
+	if (value[index++] != ',')
+		return (0);
+	if (!parse_rgb_component(value, &index, &color->g))
+		return (0);
+	if (value[index++] != ',')
+		return (0);
+	if (!parse_rgb_component(value, &index, &color->b))
+		return (0);
+	if (value[index])
+		return (0);
+	color->value = color_to_int(color);
+	return (1);
+}
+
+int	parse_color(char *line, t_scene *scene)
+{
+	int		index;
+	int		start;
+	t_color	*color;
+	char	*value;
+
+	if (!line || !scene || (line[0] != ID_F[0] && line[0] != ID_C[0]))
+		return (print_error("invalid color line"), 0);
+	color = &scene->floor;
+	if (line[0] == ID_C[0])
+		color = &scene->ceiling;
+	if (color->value != -1)
+		return (print_error("duplicate color identifier"), 0);
+	index = skip_color_spaces(line, 1);
+	start = index;
+	while (line[index] && !ft_isspace_cub(line[index]))
+		index++;
+	if (index == start)
+		return (print_error("missing color value"), 0);
+	value = ft_substr(line, start, index - start);
+	if (!value)
+		return (print_error("memory allocation failed"), 0);
+	index = skip_color_spaces(line, index);
+	if (line[index] || !parse_rgb(value, color))
+		return (free(value), print_error("invalid color value"), 0);
+	free(value);
+	return (1);
+}
--- src/parser/parse_map.c
+++ src/parser/parse_map.c
@@ -10,3 +10,73 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../../includes/parser.h"
+#include "../../includes/utils.h"
+#include "../../includes/libft.h"
+
+static int	count_map_height(char **lines, int start)
+{
+	int	height;
+
+	height = 0;
+	while (lines[start + height])
+		height++;
+	return (height);
+}
+
+static int	get_map_width(char **lines, int start)
+{
+	int	width;
+	int	line_width;
+	int	index;
+
+	width = 0;
+	index = start;
+	while (lines[index])
+	{
+		line_width = ft_strlen(lines[index]);
+		if (line_width > width)
+			width = line_width;
+		index++;
+	}
+	return (width);
+}
+
+int	copy_map_lines(char **lines, int start, t_map *map)
+{
+	int	index;
+
+	index = 0;
+	while (index < map->height)
+	{
+		map->grid[index] = ft_strdup(lines[start + index]);
+		if (!map->grid[index])
+		{
+			map->grid[index] = NULL;
+			return (0);
+		}
+		index++;
+	}
+	map->grid[index] = NULL;
+	return (1);
+}
+
+int	parse_map(char **lines, int start, t_scene *scene)
+{
+	if (!lines || start < 0 || !scene)
+		return (print_error("missing map"));
+	scene->map.height = count_map_height(lines, start);
+	if (scene->map.height <= 0)
+		return (print_error("missing map"));
+	scene->map.width = get_map_width(lines, start);
+	scene->map.grid = malloc(sizeof(char *) * (scene->map.height + 1));
+	if (!scene->map.grid)
+		return (print_error("memory allocation failed"));
+	if (!copy_map_lines(lines, start, &scene->map))
+	{
+		free_split(scene->map.grid);
+		scene->map.grid = NULL;
+		return (print_error("memory allocation failed"));
+	}
+	return (1);
+}
