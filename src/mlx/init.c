0fe4fb5f20b64a04db1b21b3786549f697aecf4a
Dk Siti Nur Sabrina Pg Md Noor Sufian <dkpg-md-@c4b9c4.42berlin.de>
2026-09-13 18:39:04 +0200

    Norm checked: Initialise mlx and cleanup

--- src/mlx/init.c
+++ src/mlx/init.c
@@ -1,4 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dkpg-md- <dkpg-md-@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/13 18:37:39 by dkpg-md-          #+#    #+#             */
+/*   Updated: 2026/09/13 18:37:41 by dkpg-md-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../includes/raycast.h"
+#include "../../includes/cub3d.h"
 
 void	init_game(t_game *game)
 {
@@ -17,6 +30,14 @@ void	init_game(t_game *game)
 
 void	cleanup_game(t_game *game)
 {
+	if (game->tex_n.img)
+		mlx_destroy_image(game->mlx, game->tex_n.img);
+	if (game->tex_s.img)
+		mlx_destroy_image(game->mlx, game->tex_s.img);
+	if (game->tex_e.img)
+		mlx_destroy_image(game->mlx, game->tex_e.img);
+	if (game->tex_w.img)
+		mlx_destroy_image(game->mlx, game->tex_w.img);
 	if (game->screen.img)
 		mlx_destroy_image(game->mlx, game->screen.img);
 	if (game->win)
@@ -26,4 +47,24 @@ void	cleanup_game(t_game *game)
 		mlx_destroy_display(game->mlx);
 		free(game->mlx);
 	}
+}
+
+static void	load_texture(t_game *game, t_img *tex, char *path)
+{
+	int	width;
+	int	height;
+
+	tex->img = mlx_xpm_file_to_image(game->mlx, path, &width, &height);
+	if (!tex->img)
+		exit(1);
+	tex->addr = mlx_get_data_addr(tex->img, &tex->bpp, &tex->line_len,
+			&tex->endian);
+}
+
+void	init_textures(t_game *game)
+{
+	load_texture(game, &game->tex_n, game->scene.textures.north_path);
+	load_texture(game, &game->tex_s, game->scene.textures.south_path);
+	load_texture(game, &game->tex_e, game->scene.textures.east_path);
+	load_texture(game, &game->tex_w, game->scene.textures.west_path);
 }
