358684aa6087169472f56eefbaa8cfd62c267ac2
Dk Siti Nur Sabrina Pg Md Noor Sufian <dkpg-md-@c4b9c4.42berlin.de>
2026-09-13 18:50:01 +0200

    Norm checked: fixed misaligned variable declaration

--- includes/player.h
+++ includes/player.h
@@ -3,17 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   player.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: makassa <makassa@student.42berlin.de>      +#+  +:+       +#+        */
+/*   By: dkpg-md- <dkpg-md-@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/25 14:29:02 by makassa           #+#    #+#             */
-/*   Updated: 2026/08/25 14:31:25 by makassa          ###   ########.fr       */
+/*   Updated: 2026/09/13 18:49:40 by dkpg-md-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PLAYER_H
 # define PLAYER_H
 
+# include "cub3d.h"
 # include "vector.h"
+# include <stdbool.h>
+
+typedef struct s_hooks
+{
+	bool	w;
+	bool	a;
+	bool	s;
+	bool	d;
+	bool	left;
+	bool	right;
+}	t_hooks;
 
 typedef struct s_player
 {
@@ -21,6 +33,7 @@ typedef struct s_player
 	t_vec	dir;
 	t_vec	plane;
 	char	orientation;
+	t_hooks	hooks;
 }			t_player;
 
 #endif
