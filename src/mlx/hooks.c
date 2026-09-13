474b537b54c5796e04eb1e2c0091872be8f83b1f
Dk Siti Nur Sabrina Pg Md Noor Sufian <dkpg-md-@c4b9c4.42berlin.de>
2026-09-13 18:39:31 +0200

    Norm checked: Key hooks

--- src/mlx/hooks.c
+++ src/mlx/hooks.c
@@ -1,7 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   hooks.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dkpg-md- <dkpg-md-@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/13 18:37:47 by dkpg-md-          #+#    #+#             */
+/*   Updated: 2026/09/13 18:37:50 by dkpg-md-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../includes/raycast.h"
 #include "../../includes/utils.h"
-
-static int	g_keys[65536];
 
 int	close_hook(t_game *game)
 {
@@ -13,54 +23,42 @@ int	close_hook(t_game *game)
 
 int	key_press_hook(int keycode, t_game *game)
 {
+	t_hooks	*hooks;
+
+	hooks = &game->scene.player.hooks;
 	if (keycode == KEY_ESC)
 		close_hook(game);
-	if (keycode >= 0 && keycode < 65536)
-		g_keys[keycode] = 1;
+	else if (keycode == KEY_W)
+		hooks->w = true;
+	else if (keycode == KEY_A)
+		hooks->a = true;
+	else if (keycode == KEY_S)
+		hooks->s = true;
+	else if (keycode == KEY_D)
+		hooks->d = true;
+	else if (keycode == KEY_LEFT)
+		hooks->left = true;
+	else if (keycode == KEY_RIGHT)
+		hooks->right = true;
 	return (0);
 }
 
 int	key_release_hook(int keycode, t_game *game)
 {
-	(void)game;
-	if (keycode >= 0 && keycode < 65536)
-		g_keys[keycode] = 0;
+	t_hooks	*hooks;
+
+	hooks = &game->scene.player.hooks;
+	if (keycode == KEY_W)
+		hooks->w = false;
+	else if (keycode == KEY_A)
+		hooks->a = false;
+	else if (keycode == KEY_S)
+		hooks->s = false;
+	else if (keycode == KEY_D)
+		hooks->d = false;
+	else if (keycode == KEY_LEFT)
+		hooks->left = false;
+	else if (keycode == KEY_RIGHT)
+		hooks->right = false;
 	return (0);
-}
-
-void	handle_input(t_game *game)
-{
-	double	move_x;
-	double	move_y;
-	t_vec	*dir;
-
-	dir = &game->scene.player.dir;
-	move_x = 0;
-	move_y = 0;
-	if (g_keys[KEY_W])
-	{
-		move_x += dir->x * MOVE_SPEED;
-		move_y += dir->y * MOVE_SPEED;
-	}
-	if (g_keys[KEY_S])
-	{
-		move_x -= dir->x * MOVE_SPEED;
-		move_y -= dir->y * MOVE_SPEED;
-	}
-	if (g_keys[KEY_A])
-	{
-		move_x += dir->y * MOVE_SPEED;
-		move_y -= dir->x * MOVE_SPEED;
-	}
-	if (g_keys[KEY_D])
-	{
-		move_x -= dir->y * MOVE_SPEED;
-		move_y += dir->x * MOVE_SPEED;
-	}
-	if (move_x != 0 || move_y != 0)
-		move_player(game, move_x, move_y);
-	if (g_keys[KEY_LEFT])
-		rotate_player(game, -ROT_SPEED);
-	if (g_keys[KEY_RIGHT])
-		rotate_player(game, ROT_SPEED);
 }
