826c78b637e35ae7b5aaebba21b18f23239c1f53
Dk Siti Nur Sabrina Pg Md Noor Sufian <dkpg-md-@c4b9c4.42berlin.de>
2026-09-13 18:19:50 +0200

    Norm checked: extracted texture.c, rendering is for drawing the columand putting pixels on screen

--- src/rendering/render.c
+++ src/rendering/render.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   render.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dkpg-md- <dkpg-md-@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/13 16:42:34 by dkpg-md-          #+#    #+#             */
+/*   Updated: 2026/09/13 18:15:30 by dkpg-md-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "raycast.h"
 
 void	put_pixel(t_img *img, int x, int y, int color)
@@ -25,50 +37,24 @@ void	calc_wall_height(t_ray *ray)
 		ray->draw_end = WIN_HEIGHT - 1;
 }
 
-static t_img	*pick_texture(t_game *game, t_ray *ray)
-{
-	if (ray->side == 0)
-	{
-		if (ray->ray_dir_x > 0)
-			return (&game->tex_w);
-		return (&game->tex_e);
-	}
-	if (ray->ray_dir_y > 0)
-		return (&game->tex_n);
-	return (&game->tex_s);
-}
-
-static int	sample_texture(t_img *tex, int tex_x, int tex_y)
-{
-	char	*pixel;
-
-	pixel = tex->addr + (tex_y * tex->line_len + tex_x * (tex->bpp / 8));
-	return (*(unsigned int *)pixel);
-}
-
 void	draw_column(t_game *game, t_ray *ray, int x)
 {
 	t_img	*tex;
 	int		tex_x;
 	int		tex_y;
 	int		y;
-	double	step;
 	double	tex_pos;
 
-	tex = pick_texture(game, ray);
-	tex_x = (int)(ray->wall_x * TEX_WIDTH);
-	if ((ray->side == 0 && ray->ray_dir_x > 0)
-		|| (ray->side == 1 && ray->ray_dir_y < 0))
-		tex_x = TEX_WIDTH - tex_x - 1;
+	tex = get_tex_column(game, ray, &tex_x);
 	y = 0;
 	while (y < ray->draw_start)
 		put_pixel(&game->screen, x, y++, game->scene.ceiling.value);
-	step = (double)TEX_HEIGHT / ray->line_height;
-	tex_pos = (ray->draw_start - WIN_HEIGHT / 2 + ray->line_height / 2) * step;
+	tex_pos = (ray->draw_start - WIN_HEIGHT / 2 + ray->line_height / 2)
+		* ((double)TEX_HEIGHT / ray->line_height);
 	while (y < ray->draw_end)
 	{
 		tex_y = (int)tex_pos & (TEX_HEIGHT - 1);
-		tex_pos += step;
+		tex_pos += (double)TEX_HEIGHT / ray->line_height;
 		put_pixel(&game->screen, x, y++, sample_texture(tex, tex_x, tex_y));
 	}
 	while (y < WIN_HEIGHT)
